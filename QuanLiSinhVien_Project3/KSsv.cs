c36742cb195a21e823f97d856a482d08dac09e45
huyhochanh002 <71912506+huyhochanh002@users.noreply.github.com>
2021-12-14 21:08:15 +0700

    Huy push test text sinhvien

--- QuanLiSinhVien_Project3/KSsv.cs
+++ QuanLiSinhVien_Project3/KSsv.cs
@@ -9,7 +9,10 @@ using System.Windows.Forms;
 namespace QuanLiSinhVien_Project3
 {
     public class KSsv
-    {
+    { /// <summary>
+    /// /File này chỉ đọc và lấy dữ liệu không có thêm sữa xóa , muốn thêm sữa xóa thì làm trên file text để trong "bin" đó nha sau đó gọi ra file sinhvien text đó
+    /// làm chung cho đồng bộ
+    /// </summary>
         public static List<SinhVien.SinhVien> ListSV = new List<SinhVien.SinhVien>();
         public static string pathfile = string.Format(@"{0}\Sinhvien.txt", Application.StartupPath);
         public void GetNguoi(string path)
