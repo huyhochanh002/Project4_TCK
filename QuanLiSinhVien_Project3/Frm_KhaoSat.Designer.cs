24a977d9e9908103ae021d269e010d05f8fd5deb
huyhochanh002 <71912506+huyhochanh002@users.noreply.github.com>
2021-12-23 11:26:26 +0700

    huy hfix refresh

--- QuanLiSinhVien_Project3/Frm_KhaoSat.Designer.cs
+++ QuanLiSinhVien_Project3/Frm_KhaoSat.Designer.cs
@@ -29,8 +29,8 @@ namespace QuanLiSinhVien_Project3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.pnkhaosat_full = new System.Windows.Forms.Panel();
+            this.btn_xuatexks = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.datasinhviendihoc = new System.Windows.Forms.DataGridView();
@@ -47,8 +47,7 @@ namespace QuanLiSinhVien_Project3
             this.colTT = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.kiemSinhvVienNghi1 = new QuanLiSinhVien_Project3.Usercontrol.KiemSinhvVienNghi();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.refresh = new System.Windows.Forms.Timer(this.components);
-            this.btn_xuatexks = new System.Windows.Forms.Button();
+            this.bth_refesh = new System.Windows.Forms.Button();
             this.pnkhaosat_full.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.datasinhviendihoc)).BeginInit();
             this.SuspendLayout();
@@ -56,6 +55,7 @@ namespace QuanLiSinhVien_Project3
             // pnkhaosat_full
             // 
             this.pnkhaosat_full.BackColor = System.Drawing.Color.SkyBlue;
+            this.pnkhaosat_full.Controls.Add(this.bth_refesh);
             this.pnkhaosat_full.Controls.Add(this.btn_xuatexks);
             this.pnkhaosat_full.Controls.Add(this.button2);
             this.pnkhaosat_full.Controls.Add(this.button1);
@@ -66,6 +66,17 @@ namespace QuanLiSinhVien_Project3
             this.pnkhaosat_full.Name = "pnkhaosat_full";
             this.pnkhaosat_full.Size = new System.Drawing.Size(800, 450);
             this.pnkhaosat_full.TabIndex = 0;
+            // 
+            // btn_xuatexks
+            // 
+            this.btn_xuatexks.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_xuatexks.Location = new System.Drawing.Point(505, 124);
+            this.btn_xuatexks.Name = "btn_xuatexks";
+            this.btn_xuatexks.Size = new System.Drawing.Size(120, 44);
+            this.btn_xuatexks.TabIndex = 4;
+            this.btn_xuatexks.Text = "Xuất Excel";
+            this.btn_xuatexks.UseVisualStyleBackColor = true;
+            this.btn_xuatexks.Click += new System.EventHandler(this.btn_xuatexks_Click);
             // 
             // button2
             // 
@@ -222,26 +233,21 @@ namespace QuanLiSinhVien_Project3
             this.kiemSinhvVienNghi1.BackColor = System.Drawing.Color.SkyBlue;
             this.kiemSinhvVienNghi1.Dock = System.Windows.Forms.DockStyle.Top;
             this.kiemSinhvVienNghi1.Location = new System.Drawing.Point(0, 0);
-            this.kiemSinhvVienNghi1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.kiemSinhvVienNghi1.Margin = new System.Windows.Forms.Padding(4);
             this.kiemSinhvVienNghi1.Name = "kiemSinhvVienNghi1";
             this.kiemSinhvVienNghi1.Size = new System.Drawing.Size(800, 198);
             this.kiemSinhvVienNghi1.TabIndex = 0;
             // 
-            // refresh
+            // bth_refesh
             // 
-            this.refresh.Interval = 30000;
-            this.refresh.Tick += new System.EventHandler(this.refresh_Tick);
-            // 
-            // btn_xuatexks
-            // 
-            this.btn_xuatexks.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_xuatexks.Location = new System.Drawing.Point(505, 124);
-            this.btn_xuatexks.Name = "btn_xuatexks";
-            this.btn_xuatexks.Size = new System.Drawing.Size(120, 44);
-            this.btn_xuatexks.TabIndex = 4;
-            this.btn_xuatexks.Text = "Xuất Excel";
-            this.btn_xuatexks.UseVisualStyleBackColor = true;
-            this.btn_xuatexks.Click += new System.EventHandler(this.btn_xuatexks_Click);
+            this.bth_refesh.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bth_refesh.Location = new System.Drawing.Point(641, 124);
+            this.bth_refesh.Name = "bth_refesh";
+            this.bth_refesh.Size = new System.Drawing.Size(112, 44);
+            this.bth_refesh.TabIndex = 5;
+            this.bth_refesh.Text = "Làm Mới";
+            this.bth_refesh.UseVisualStyleBackColor = true;
+            this.bth_refesh.Click += new System.EventHandler(this.bth_refesh_Click);
             // 
             // Frm_KhaoSat
             // 
@@ -265,7 +271,6 @@ namespace QuanLiSinhVien_Project3
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.DataGridView datasinhviendihoc;
         private Usercontrol.KiemSinhvVienNghi kiemSinhvVienNghi1;
-        private System.Windows.Forms.Timer refresh;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridViewTextBoxColumn colID;
@@ -280,5 +285,6 @@ namespace QuanLiSinhVien_Project3
         private System.Windows.Forms.DataGridViewTextBoxColumn colHDT;
         private System.Windows.Forms.DataGridViewTextBoxColumn colTT;
         private System.Windows.Forms.Button btn_xuatexks;
+        private System.Windows.Forms.Button bth_refesh;
     }
 }
